147e9a9843e27ab0808cb7bc8cbb0b066d6579a7
TanzidKaiser <tanzid.888@gmail.com>
2018-01-23 17:06:41 +0600

    getEmp

--- EFCodeFirstStoreProcedure/Controllers/HomeController.cs
+++ EFCodeFirstStoreProcedure/Controllers/HomeController.cs
@@ -3,27 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using EFCodeFirstStoreProcedure.Models;
 namespace EFCodeFirstStoreProcedure.Controllers
 {
     public class HomeController : Controller
     {
+        StoreProcedureDBEntities db = new StoreProcedureDBEntities();
+        // GET: Home
         public ActionResult Index()
         {
+            var data = db.Database.SqlQuery<Employee>("exec sp_getEmp");
+            return View(data);
+        }  
+        [HttpGet]
+        public ActionResult Create()
+        {
             return View();
         }
-
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Create(Employee model)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
